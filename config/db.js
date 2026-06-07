0873e6d6bbff7be4067a3f9611c07fbcc38a96b9
richad <113709320+richad1109@users.noreply.github.com>
2026-06-08 04:04:22 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true);
+console.log("MONGODB:", process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log("MongoDB Connected ✔");
-})
-.catch((err) => {
-    console.log("MongoDB Connection Error ❌");
-    console.log(err);
-});
-
-module.exports = mongoose;
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("Mongo Error:", err));
